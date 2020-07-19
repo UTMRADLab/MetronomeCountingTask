8741dfe3808403b225a02cd8bec5c686a438f609
JoshuaConcon <conconjoshua@gmail.com>
2020-07-19 17:27:21 -0400

    Starter for Game Logic

---
+++ frontend/src/main/game/gameLogic.js
@@ -0,0 +1,16 @@
+
+const success = () => {
+  alert("correct!");
+};
+
+const gameCatch = () => {
+  alert("went over, caught by game");
+};
+
+const undercount = () => {
+  alert("undercounting");
+};
+
+const selfCatch = () => {
+  alert("self caught, the game will now reset");
+};
--- frontend/src/main/game/index.js
+++ frontend/src/main/game/index.js
@@ -1,24 +1,13 @@
 import React, {Component} from 'react';
 import './game.css'
-import Anime from 'react-anime';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowUp, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 class Game extends Component {
   render() {
     return(
       <div>
-        <p>test</p>
-        <FontAwesomeIcon icon={faArrowUp} size="3x" />
-        <br />
-        <br />
-        <FontAwesomeIcon icon={faArrowRight} size="3x"/>
-        <br />
-        <br />
-        <Anime direction='alternate'
-        loop={true}
-        scale={[.5, 1]}>
-          <div className="ball" />
-        </Anime>
+        <p>instructions</p>
+        <p>right arrow goes to the next round</p>
+        <p>down arrow self catches</p>
+        <p>up arrow tells us when you're done</p>
       </div>
     );
   }
