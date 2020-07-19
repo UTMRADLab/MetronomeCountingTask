bd3b0572209ad6a99ae27a0ee9a8ce8e53a0e869
JoshuaConcon <conconjoshua@gmail.com>
2020-07-19 18:33:30 -0400

    Connecting Logic

--- frontend/src/main/game/gameLogic.js
+++ frontend/src/main/game/gameLogic.js
@@ -1,3 +1,8 @@
+const successTarget = 20;
+const selfCatchTarget = 30;
+let gameCount = 0;
+let usrCount = 0;
+let roundActive = false;
 
 const success = () => {
   alert("correct!");
@@ -7,10 +12,46 @@ const gameCatch = () => {
   alert("went over, caught by game");
 };
 
-const undercount = () => {
+const overcountMiss = () => {
+  alert("overcounting");
+};
+
+const undercountMiss = () => {
   alert("undercounting");
 };
 
 const selfCatch = () => {
   alert("self caught, the game will now reset");
+};
+
+const roundMissed = () => {
+  alert("round missed!");
+};
+
+const playNextRoundSound = () => {
+
+};
+
+const nextRound = () => {
+  if(usrCount !== gameCount) roundMissed();
+  gameCount++;
+  playNextRoundSound();
+  roundActive = true;
+};
+
+const reactPress = () => {
+  if(roundActive) {
+    usrCount++;
+    roundActive = false;
+  }
+};
+
+const confirmDonePress = () => {
+  if(gameCount === usrCount === successTarget) {
+    success();
+  } else if (usrCount < successTarget) {
+    undercountMiss();
+  } else if (usrCount > successTarget) {
+    overcountMiss();
+  }
 };
