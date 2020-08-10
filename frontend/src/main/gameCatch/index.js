46d76626db6e29377062d5735887076e756b4261
JoshuaConcon <conconjoshua@gmail.com>
2020-08-10 06:31:29 -0400

    Adding reasons for catching

--- frontend/src/main/gameCatch/index.js
+++ frontend/src/main/gameCatch/index.js
@@ -9,6 +9,17 @@ class GameCatch extends Component {
           caught by game!
         </p>
         <p>
+          please specify why the game caught you
+        </p>
+        <input type="radio" id="miscounted" name="reason" value="miscounted" />
+        <label for="miscounted">miscounted</label>
+        <br />
+        <input type="radio" id="tired" name="reason" value="tired" />
+        <label for="tired">got tired</label>
+        <br />
+        <input type="radio" id="spaced" name="reason" value="spaced" />
+        <label for="spaced">spaced out</label>
+        <p>
           <Link to="/game">Return to Game</Link>
         </p>
       </div>
--- frontend/src/main/selfCatch/index.js
+++ frontend/src/main/selfCatch/index.js
@@ -11,6 +11,14 @@ class SelfCatch extends Component {
         <p>
           please specify why you self caught
         </p>
+        <input type="radio" id="miscounted" name="reason" value="miscounted" />
+        <label for="miscounted">miscounted</label>
+        <br />
+        <input type="radio" id="tired" name="reason" value="tired" />
+        <label for="tired">got tired</label>
+        <br />
+        <input type="radio" id="spaced" name="reason" value="spaced" />
+        <label for="spaced">spaced out</label>
         <p>
           <Link to="/game">Return to Game</Link>
         </p>
