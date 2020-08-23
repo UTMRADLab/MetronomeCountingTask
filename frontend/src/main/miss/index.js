d1d4c8c226963acafa9550a6cd68417e57eecaaf
JoshuaConcon <conconjoshua@gmail.com>
2020-08-23 15:38:54 -0400

    Radio Buttons State is stored

--- frontend/src/main/miss/index.js
+++ frontend/src/main/miss/index.js
@@ -2,6 +2,21 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
 class Miss extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reason: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      reason: event.target.value
+    });
+  }
+
   render() {
     return(
       <div>
@@ -11,6 +26,42 @@ class Miss extends Component {
         <p>
           you pressed done too early!
         </p>
+        <p>
+          please specify why you missed
+        </p>
+        <br />
+        <input
+          type="radio"
+          id="tired"
+          name="reason"
+          value="tired"
+          checked={this.state.reason === "tired"}
+          onChange={this.handleChange}/>
+        <label htmlFor="tired">
+          got tired
+        </label>
+        <br />
+        <input
+          type="radio"
+          id="spaced"
+          name="reason"
+          value="spaced"
+          checked={this.state.reason === "spaced"}
+          onChange={this.handleChange}/>
+        <label htmlFor="spaced">
+          spaced out
+        </label>
+        <br />
+        <input
+          type="radio"
+          id="miscounted"
+          name="reason"
+          value="miscounted"
+          checked={this.state.reason === "miscounted"}
+          onChange={this.handleChange}/>
+        <label htmlFor="miscounted">
+          miscounted
+        </label>
         <p>
           <Link to="/game">Return to Game</Link>
         </p>
--- frontend/src/main/selfCatch/index.js
+++ frontend/src/main/selfCatch/index.js
@@ -2,6 +2,21 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
 class SelfCatch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reason: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      reason: event.target.value
+    });
+  }
+
   render() {
     return(
       <div>
@@ -11,14 +26,39 @@ class SelfCatch extends Component {
         <p>
           please specify why you self caught
         </p>
-        <input type="radio" id="miscounted" name="reason" value="miscounted" />
-        <label htmlFor="miscounted">miscounted</label>
         <br />
-        <input type="radio" id="tired" name="reason" value="tired" />
-        <label htmlFor="tired">got tired</label>
+        <input
+          type="radio"
+          id="tired"
+          name="reason"
+          value="tired"
+          checked={this.state.reason === "tired"}
+          onChange={this.handleChange}/>
+        <label htmlFor="tired">
+          got tired
+        </label>
         <br />
-        <input type="radio" id="spaced" name="reason" value="spaced" />
-        <label htmlFor="spaced">spaced out</label>
+        <input
+          type="radio"
+          id="spaced"
+          name="reason"
+          value="spaced"
+          checked={this.state.reason === "spaced"}
+          onChange={this.handleChange}/>
+        <label htmlFor="spaced">
+          spaced out
+        </label>
+        <br />
+        <input
+          type="radio"
+          id="miscounted"
+          name="reason"
+          value="miscounted"
+          checked={this.state.reason === "miscounted"}
+          onChange={this.handleChange}/>
+        <label htmlFor="miscounted">
+          miscounted
+        </label>
         <p>
           <Link to="/game">Return to Game</Link>
         </p>
